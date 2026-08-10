151d914f69558e7c3eae8ffb29cc831ed0be1d97
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2026-08-10 16:49:25 +0100

    CDMS-1521: Update floci, trade-gateway and trade-gateway-publisher services

--- scripts/floci/ready.d/99-ready.sh
+++ scripts/floci/ready.d/99-ready.sh
@@ -5,14 +5,20 @@ function is_ready() {
   awslocal s3 ls s3://reports || return 1
 
   # trade-gateway
-  awslocal sns list-topics --query "Topics[?ends_with(TopicArn, ':trade_gateway_ched_updates')].TopicArn" || return 1
-  awslocal sns list-topics --query "Topics[?ends_with(TopicArn, ':trade_gateway_docom_updates')].TopicArn" || return 1
-  awslocal sns list-topics --query "Topics[?ends_with(TopicArn, ':trade_gateway_intra_updates')].TopicArn" || return 1
+  awslocal sns get-topic-attributes --topic-arn "arn:aws:sns:$AWS_REGION:000000000000:trade_gateway_ched_updates" >/dev/null || return 1
+  awslocal sns get-topic-attributes --topic-arn "arn:aws:sns:$AWS_REGION:000000000000:trade_gateway_docom_updates" >/dev/null || return 1
+  awslocal sns get-topic-attributes --topic-arn "arn:aws:sns:$AWS_REGION:000000000000:trade_gateway_intra_updates" >/dev/null || return 1
+
+  # trade-gateway-publisher
+  awslocal sns get-topic-attributes --topic-arn "arn:aws:sns:$AWS_REGION:000000000000:trade_gateway_publisher_ched_stream_internal.fifo" >/dev/null || return 1
+  awslocal sns get-topic-attributes --topic-arn "arn:aws:sns:$AWS_REGION:000000000000:trade_gateway_publisher_ched_updates.fifo" >/dev/null || return 1
+  awslocal sns get-topic-attributes --topic-arn "arn:aws:sns:$AWS_REGION:000000000000:trade_gateway_publisher_intra_stream_internal.fifo" >/dev/null || return 1
+  awslocal sns get-topic-attributes --topic-arn "arn:aws:sns:$AWS_REGION:000000000000:trade_gateway_publisher_intra_updates.fifo" >/dev/null || return 1
+  awslocal sqs get-queue-url --queue-name trade_gateway_publisher_ched_stream_internal_publisher.fifo || return 1
+  awslocal sqs get-queue-url --queue-name trade_gateway_publisher_intra_stream_internal_publisher.fifo || return 1
+  awslocal sqs get-queue-url --queue-name trade_gateway_publisher_intra_updates_test.fifo || return 1
 
   return 0
 }
 
-while ! is_ready; do
-  echo "Waiting until ready..."
-  sleep 1
-done
+is_ready
