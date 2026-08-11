2c00c696cb482a1361fa96c6fffe3e1010c3a784
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2026-08-11 20:55:52 +0100

    CMDS-1521: Update Floci scripts (#11)

--- scripts/floci/ready.d/99-ready.sh
+++ scripts/floci/ready.d/99-ready.sh
@@ -14,8 +14,14 @@ function is_ready() {
   awslocal sns get-topic-attributes --topic-arn "arn:aws:sns:$AWS_REGION:000000000000:trade_gateway_publisher_ched_updates.fifo" >/dev/null || return 1
   awslocal sns get-topic-attributes --topic-arn "arn:aws:sns:$AWS_REGION:000000000000:trade_gateway_publisher_intra_stream_internal.fifo" >/dev/null || return 1
   awslocal sns get-topic-attributes --topic-arn "arn:aws:sns:$AWS_REGION:000000000000:trade_gateway_publisher_intra_updates.fifo" >/dev/null || return 1
+  awslocal sqs get-queue-url --queue-name trade_gateway_publisher_ched_stream_internal_publisher-deadletter.fifo || return 1
   awslocal sqs get-queue-url --queue-name trade_gateway_publisher_ched_stream_internal_publisher.fifo || return 1
+  awslocal sqs get-queue-url --queue-name trade_gateway_publisher_ched_stream_internal_asb_publisher-deadletter.fifo || return 1
+  awslocal sqs get-queue-url --queue-name trade_gateway_publisher_ched_stream_internal_asb_publisher.fifo || return 1
+  awslocal sqs get-queue-url --queue-name trade_gateway_publisher_intra_stream_internal_publisher-deadletter.fifo || return 1
   awslocal sqs get-queue-url --queue-name trade_gateway_publisher_intra_stream_internal_publisher.fifo || return 1
+  awslocal sqs get-queue-url --queue-name trade_gateway_publisher_intra_stream_internal_asb_publisher-deadletter.fifo || return 1
+  awslocal sqs get-queue-url --queue-name trade_gateway_publisher_intra_stream_internal_asb_publisher.fifo || return 1
   awslocal sqs get-queue-url --queue-name trade_gateway_publisher_intra_updates_test.fifo || return 1
 
   return 0
